b2b5c894746ca0fa0528a5f2c2bf5298343981b2
Byung Ho Lee <byungholee@Byungs-MacBook-Pro.local>
2020-03-21 17:22:32 +0900

    Add 16personalities to home page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>“It’s so incredible to finally be understood.”</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Take our Personality Test and get a “freakishly accurate” description
+          of who you are and why you do things the way you do.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.16personalities.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          16personalities
         </a>
       </header>
     </div>
